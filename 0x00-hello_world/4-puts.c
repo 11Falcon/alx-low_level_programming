bf49d4230c7e449d676421cf3b72932b1c34e413
soufiane <soufian9mkns@gmail.com>
2023-07-13 11:32:52 -0700

    printing using the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-/** smiyti soufiane falcon*/
+	/**
+	 * This is the preferred style for multi-line
+	 * comments in C source code.
+	 * Please use it consistently.
+	 *
+	 * Description:  A column of asterisks on the left side,
+	 * with beginning and ending almost-blank lines.
+	 */
 int main(void)
 {
 	/* Entry point of the program*/

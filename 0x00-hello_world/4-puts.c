0a4239a4397cd149f28edfce5d693baebdc1ec75
soufiane <soufian9mkns@gmail.com>
2023-07-13 11:27:29 -0700

    printing using the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* smiyti soufiane falcon*/
 int main(void)
 {
-	// Entry point of the program
+	/* Entry point of the program*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

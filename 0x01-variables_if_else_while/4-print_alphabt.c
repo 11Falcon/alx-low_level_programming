01cb93fd4edb1e764cab6e19b4befd5b1c04e72d
soufiane <soufian9mkns@gmail.com>
2023-07-14 09:58:55 -0700

    exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+        int i;
+        char s;
+
+        i = 97;
+        while (i < 123)
+        {
+                s = i;
+		if (s!='q' && s!='e')
+			putchar(s);
+                i++;
+        }
+        putchar('\n');
+        return (0);
+}

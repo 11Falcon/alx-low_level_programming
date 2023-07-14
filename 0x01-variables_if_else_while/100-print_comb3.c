2816c90d857012d949f829525ce1da939eb7ceaa
soufiane <soufian9mkns@gmail.com>
2023-07-14 15:44:11 -0700

    combine

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alyaws 0(Success)
+ */
+int main(void)
+{
+	int i,j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = ; j <= 9 ; j++)
+		{
+			if(j != i)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(',');
+				putchr(' ');
+			}
+		}
+	}
+}
+

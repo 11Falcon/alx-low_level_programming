317dc1321ef684c62749399ad0d21404565f0331
soufiane <soufian9mkns@gmail.com>
2023-07-14 07:37:58 -0700

    conditions about the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
 int main(void)
 {
 	int n, falcon;
@@ -12,9 +14,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	falcon = n % 10;
 	printf("Last digit of %d is %d ", n, falcon);
-	if (falcon > 5 )
+	if (falcon > 5)
 		printf("and is greater than 5\n");
-	else if (falcon == 0 )
+	else if (falcon == 0)
 		printf("and is 0\n");
 	else
 		printf("and is less than 6 and not 0\n");

d631b7814005f21c209fe10aff08b29db6d7b52a
soufiane <soufian9mkns@gmail.com>
2023-07-14 07:52:29 -0700

    .

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int i;
+	char s;
+	i=97;
+	while(i < 123)
+	{
+		s=i;
+		putchar(s);
+		i++;
+	}
+}
+

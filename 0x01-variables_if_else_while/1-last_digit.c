bf08a18902ad8cdc1e9808ff1be9ef6723265e34
soufiane <soufian9mkns@gmail.com>
2023-07-14 07:35:19 -0700

    conditions about the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,15 +6,15 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n,falcon;
+	int n, falcon;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	falcon = n % 10 ;
-	printf("Last digit of %d is %d", n,falcon);
+	falcon = n % 10;
+	printf("Last digit of %d is %d ", n, falcon);
 	if (falcon > 5 )
 		printf("and is greater than 5\n");
-	else if (falcon == 0)
+	else if (falcon == 0 )
 		printf("and is 0\n");
 	else
 		printf("and is less than 6 and not 0\n");

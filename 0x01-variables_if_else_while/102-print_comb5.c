689dd65fb1f6c6da5f51b1aa674ffe9862579446
soufiane <soufian9mkns@gmail.com>
2023-07-14 18:22:23 -0700

    the End

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alyaws 0(Success)
+ */
+int main(void)
+{
+	int i, s, l;
+	int j, d, m;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			s = i % 10;
+			d = i - s;
+			d = d / 10;
+			putchar('s');
+			putchar('d');
+			putchar(' ');
+			l = j % 10;
+			m = j - l;
+			m = m / 10;
+			putchar('l');
+			putchar('m');
+			if((i != 98) && (j != 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

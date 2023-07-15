7b7c2aaf3a938acbb358afab4f55ae4a20f42c00
soufiane <soufian9mkns@gmail.com>
2023-07-14 17:10:00 -0700

    the end

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0(Success)
  */
 int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i <=9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{

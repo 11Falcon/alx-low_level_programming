5ce900f6bcbcb89862d062694de59cdd536aa217
soufiane <soufian9mkns@gmail.com>
2023-07-18 15:09:29 -0700

    first try

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabets x 10
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		void print_alphabet();
+	}
+	

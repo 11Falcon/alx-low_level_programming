324f7f0be8c484f9d92b6ee48b7d9862b5b41fff
soufiane <soufian9mkns@gmail.com>
2023-07-18 16:28:01 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "6-abs.c"
+/**
+ * print_last_digit - last digit
+ * @s : input
+ * Return: int
+ */
+int print_last_digit(int s)
+{
+	while(_abs(c) <10)
+		c = c // 10;
+	return (c);
+}

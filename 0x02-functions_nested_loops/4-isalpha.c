341692fb5b749d4c53a63bc25503c85648eb6022
soufiane <soufian9mkns@gmail.com>
2023-07-18 15:49:39 -0700

    check if the input is an alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,6 +3,7 @@
 /**
  * _isalpha: is alphabet or not
  * @c:ckecking if the input is an alphabet
+ *
  * Return:1 if is an alphabet and 0 if not
  */
 int _isalpha(int c)
@@ -10,5 +11,5 @@ int _isalpha(int c)
 	if ((c >= 65) && (c <= 90))
 		return (1);
 	else
-		return _islower(c);
+		return (_islower(c));
 }

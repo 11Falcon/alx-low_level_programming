db8e7f5fc0c8268d7021756659400dcb2137f45b
soufiane <soufian9mkns@gmail.com>
2023-07-18 15:46:23 -0700

    yeah check if the input in an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "3-islower.c"
+/**
+ * _isalpha: is alphabet or not
+ * @c:ckecking if the input is an alphabet
+ * Return:1 if is an alphabet and 0 if not
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65) && (c <= 90))
+		return (1);
+	else
+		_islower(c);

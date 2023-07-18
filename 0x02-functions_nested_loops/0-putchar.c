6c5d46a93cebe312f5dab0f0da9b2652c8042da5
soufiane <soufian9mkns@gmail.com>
2023-07-18 08:41:48 -0700

    h'll

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include "main.h"
 #include <unistd.h>
 /**
  * main - Entry point
@@ -10,14 +8,4 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('a');
-	_putchar('r');
-	return (0);
-}
+

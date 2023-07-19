99ba1fda4406aebdbec19f9e0a5ca0711c157092
soufiane <soufian9mkns@gmail.com>
2023-07-19 04:22:14 -0700

    test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include "../0x01-variables_if_else_while/0-positive_or_negative.c"
 /**
- * positive_or_negative - testing the positivity of the input
+ * main1 - testing the positivity of the input
  * 
  * Return : 1 if positive ,0 if negative
  */
-int main(void)
+int main1(void)
 {
-	positive_or_negative();
+	main();
 }

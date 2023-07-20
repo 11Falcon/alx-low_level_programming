611772cf3022f576fdaf219a3ef23a38bd69f9d2
soufiane <soufian9mkns@gmail.com>
2023-07-20 13:46:29 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ * _isdigit - Entry code
+ * @c: input
+ *
+ * Return: 1 if input is digit ,else 0
+ */
+int _isdigit(int c)
+{
+	return isdigit(c) != 0 ? 1 : 0;
+}

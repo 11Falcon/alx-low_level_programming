63f647a72305e67661b55d6d8a48c0accee324fb
soufiane <soufian9mkns@gmail.com>
2023-07-20 13:23:39 -0700

    upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @c : input
+ * Return : 1 if c is upper ,else 0
+ */
+void main(char c)
+{
+	int i;
+
+	i = c;
+	if (i >= 65 && i <= 90)
+		return (1);
+	else
+		return (0);
+}

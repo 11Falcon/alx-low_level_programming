aab1526f73f7c2c0d387a20e3e093528c5e037ab
soufiane <soufian9mkns@gmail.com>
2023-07-20 15:05:43 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - Entry point
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		if (i == 2 || i == 4)
+		{
+			i++;
+			continue;
+		}
+		_putchar(i);
+		i++;
+	}
+}

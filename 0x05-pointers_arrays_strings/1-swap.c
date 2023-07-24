b444b9a57225d47928c3877dc055dc886c993fc4
soufiane <soufian9mkns@gmail.com>
2023-07-24 15:22:15 -0700

    swap values

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * swap_int - The function
- * @a: input a 
- * b: input b
+ * @a: input a
+ * @b: input b
  * Return: void
  */
 void swap_int(int *a, int *b)
 {
 	int c;
-	
+
 	c = *b;
 	*b = *a;
 	*a = c;

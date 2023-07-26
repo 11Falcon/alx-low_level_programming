3aaf2ee46221edf8a0abe5ef60b09921ca289bff
soufiane <soufian9mkns@gmail.com>
2023-07-26 13:50:47 -0700

    coppy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - Entry
+ * @dest: input
+ * @scr: input
+ * @n: input
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (src[i])
+		i++;
+	while (j < i)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	dest[j] = '\0';
+}

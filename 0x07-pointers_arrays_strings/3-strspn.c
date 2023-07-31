2e83494c69a9b954d50d1cfb47e4d1582b7542c1
soufiane <soufian9mkns@gmail.com>
2023-07-31 16:12:01 -0700

    str spn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,31 +1,24 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * _strspn - gets the length of a prefix substring.
- * @s: character to print
- * @accept: character
- * Return: i.
+ * _strspn - gets the length of a prefix substring
+ * @s: segment to compare bytes from
+ * @accept: string of bytes to compare with
+ * Return: number of bytes in segment s which consist of bytes from accept
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
-int count;
+	unsigned int i, j;
 
-while (*s != '\0')
-{
-	while (*accept != '\0')
+	i = 0;
+	while (s[i] != '\0')
 	{
-		if (*s == *accept)
-			break;
-			count++;
-			accept++;
-
-			s++;
-
+		j = 0;
+		while (accept[j] != '\0' && s[i] != accept[j])
+			j++;
+		if (accept[j] == '\0')
+			return (i);
+		i++;
 	}
-if (*accept == '\0')
-	break;
-
-}
-		return (count + 1);
+	return (i);
 }

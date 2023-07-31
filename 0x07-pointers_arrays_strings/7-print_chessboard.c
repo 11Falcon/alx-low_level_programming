d8a192365b6ca1c1195cebee2fb87b5931651dfb
soufiane <soufian9mkns@gmail.com>
2023-07-31 16:36:32 -0700

    7-

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -15,12 +15,6 @@ void print_chessboard(char (*a)[8])
 		col = 0;
 		while (col < 8)
 		{
-			if (col == 7)
-			{
-				_putchart(a[row][col]);
-				_putchart('\n');
-			}
-			else
 				_putchart(a[row][col]);
 			col++;
 		}

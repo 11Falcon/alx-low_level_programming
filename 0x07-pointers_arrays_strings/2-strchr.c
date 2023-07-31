85a1da57623eb25afe4dd178ae691e1987507cf7
soufiane <soufian9mkns@gmail.com>
2023-07-31 11:42:37 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,11 +5,12 @@
  * @c: input
  * Return: char
  */
-char *_strchr(char *s, char c)
-{
-	if (c[0])
-		s = c[0];
-	else
-		s = NULL;
-	return (s);
+char *_strchr(char *s, char c) {
+    while (*s != '\0') {
+        if (*s == c) {
+            return s; // Found the character, return the pointer to it
+        }
+        s++; // Move to the next character in the string
+    }
+    return NULL; // Character not found, return NULL
 }

39865b79c90ada2addce1ac4106a816e79295529
soufiane <soufian9mkns@gmail.com>
2023-07-31 16:42:08 -0700

    h

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,41 +1,51 @@
 #include "main.h"
+
 /**
- * _strncmp - compares two strings within b bytes.
- * @s1: string to check
- * @s2: string to check from
- * @b: number of bytes to check
- * Return: 0 if strings are the same
- * any other number if they are different.
+ * match_found - checks to see if the chars at a specific point in two
+ * strings are identical.
+ * @h: pointer to the first string
+ * @n: pointer to the second string
+ * Return: integer 1 for a match or 0 for no match
  */
-int _strncmp(char *s1, char *s2, unsigned int b)
+int match_found(char *h, char *n)
 {
-	unsigned int i;
-	for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
-	{
-		s1++;
-		s2++;
-	}
-	return (i - b);
+	if (*h == *n)
+		return (1);
+	else
+		return (0);
 }
+
 /**
- * _strstr - locates a substring
- * @haystack: string to look though
- * @needle: substring to find
- * Return: pointer to string with result of search
+ * _strstr - finds the first occurrence of the substring needle in the
+ * string haystack (not comparing terminating null bytes)
+ * @haystack: the string to be searched
+ * @needle: the substring to be found
+ * Return: pointer to beginning of located substring or NULL if not found
  */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int len;
-	/*Get length of needle for strncmp*/
-	len = 0;
-	while (needle[len] != '\0')
-		len++;
-	/*compare substring*/
-	while (*haystack != '\0')
+	unsigned int n, h, hold, match;
+
+	h = 0;
+	while (haystack[h] != '\0')
 	{
-		if (_strncmp(haystack, needle, len) == 0)
-			return (haystack);
-		haystack++;
+		n = 0;
+		match = 1;
+		hold = h;
+		while (match == 1 && needle[n] != '\0')
+		{
+			match = match_found((haystack + hold), (needle + n));
+			if (needle[n + 1] == '\0' && match == 1)
+			{
+				if (*needle == haystack[h])
+					return (haystack + h);
+			}
+			n++;
+			hold++;
+		}
+		h++;
 	}
-	return (NULL);
+	if (needle[0] == '\0')
+		return (haystack);
+	return ('\0');
 }

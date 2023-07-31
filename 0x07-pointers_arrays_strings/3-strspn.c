e8fc3ffe8e57a79272e53a63d37478124f0b50c6
soufiane <soufian9mkns@gmail.com>
2023-07-31 15:09:47 -0700

    str spn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,4 +7,24 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	
+	int i, j;
+	bool fa;
+
+	i = 0;
+	j = 0;
+	fa = true;
+	while ( s[i] && fa)
+	{
+		while ( accept[j])
+		{
+			if ( s[i] == accept[j])
+				{
+					i++;
+					break;
+				}
+			else
+				fa = false;
+		}
+	}
+	return (i);
+}

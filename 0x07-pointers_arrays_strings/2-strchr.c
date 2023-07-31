afb3f542fe53e0a4c0425a1bba0bbbd9424e1888
soufiane <soufian9mkns@gmail.com>
2023-07-31 11:54:12 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,13 @@
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
+	while (*s != '\0')
 	{
-            return s;
-        }
-        s++;
-    }
-    return NULL;
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
 }

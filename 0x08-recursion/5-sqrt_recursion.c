e04ef8b26fbe707e07cc7e42b23f9192824ee4e2
soufiane <soufian9mkns@gmail.com>
2023-08-03 15:41:49 -0700

    recursion sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - Entry code
+ * @n: input
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 1 && n == 0)
+		return (n);
+	int root = _sqrt_recursion(n - 1);
+
+	if (root * root <= n)
+		return (root);
+	return _sqrt_recurion(n - 1);
+}

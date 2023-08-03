3101781c0ddfdc23657118492743dbc6ed15513f
soufiane <soufian9mkns@gmail.com>
2023-08-03 13:36:44 -0700

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Entry code
+ * @s: input
+ * return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	char c;
+
+	c = *s;
+	if(!c)
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s++);
+	_putchar(c);
+

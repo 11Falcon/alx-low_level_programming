362a39ed5a914ff4608e458c4ea77747ea9b5578
soufiane <soufian9mkns@gmail.com>
2023-08-03 15:47:56 -0700

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,7 +8,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 1 && n == 0)
+	if (n == 1 || n == 0)
 		return (n);
 	if (_sqrt_recursion(n - 1) * _sqrt_recursion(n - 1) <= n)
 		return (_sqrt_recursion(n - 1));

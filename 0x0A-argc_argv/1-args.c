c101d59dd3f6bd6f6b786b99d67562af58b9d091
soufiane <soufian9mkns@gmail.com>
2023-08-06 15:30:30 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <unsintd.h>
+/**
+ * main - Entry code
+ * @argc: input
+ * @argv: input
+ * Return: int
+ */
+int main( int argc, int *argv[])
+{
+	printf("%d\n",argc);
+	void(argv);
+	return (0);
+}

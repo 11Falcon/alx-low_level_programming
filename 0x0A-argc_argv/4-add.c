ce2821fd6b654fa8b7bfbd10719b59918b1c3f83
soufiane <soufian9mkns@gmail.com>
2023-08-06 16:41:51 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,17 +14,18 @@ int main(int argc, char *argv[])
 
 	if (argc == 1)
 		printf("0\n");
-	for (i = 1;i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 	{
-			if (!isdigit(argv[i][j])){
+			if (!isdigit(argv[i][j]))
+			{
 				printf("Error\n");
 				return (1);
 			}
 	}
-	result += atoi(argv[i]);
+		result += atoi(argv[i]);
 	}
-	printf("%d\n",result);
+	printf("%d\n", result);
 	return (0);
 }

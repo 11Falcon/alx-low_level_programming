d46b824714f916f6984862bc940ad1c9a2b98845
soufiane <soufian9mkns@gmail.com>
2023-08-06 16:02:29 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry code
+ * @argc: input
+ * @argv: input
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int result = 1;
+	int i = 1;
+
+	if (argc ==3)
+	{
+		for (i = 1; i < argc ; i++)
+		{
+			result *= atoi(argv[i]);
+		}
+		printf("%d\n",result);
+		return (0);
+	}
+	else
+		{
+			printf("Error\n");
+			return (1);
+		}
+}

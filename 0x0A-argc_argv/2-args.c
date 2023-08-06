2475111fda8bf7a6e8bbea2fc2cdcc56cf24fafd
soufiane <soufian9mkns@gmail.com>
2023-08-06 15:42:39 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry code
+ * @argc: input
+ * @argv: input
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i = 0, i < argc, i++)
+	{
+		printf("%s",argv[argc]);
+	}
+	printf("\n");
+	return (0);
+}

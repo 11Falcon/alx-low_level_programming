4023b206e6c7a92afa80ef7a4db622cddb510ccb
soufiane <soufian9mkns@gmail.com>
2023-08-07 14:58:15 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - Entry code
+ * @str: input
+ * Return: cahr
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *c = (char *)malloc(sizeof(*str));
+
+	if (*str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < sizeof(*str); i++)
+	{
+		c[i] = str[i];
+	}
+	return (c);
+}

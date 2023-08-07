f09e164479d269bf0746b9dab65042d13ca029d5
soufiane <soufian9mkns@gmail.com>
2023-08-06 17:19:52 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,51 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * main - Entry code
- * @argc: input
- * @argv: input
- * Return: int
- */
-int main(int argc, char *argv[])
-{
-	int result = 0;
-	int rest = atoi(argv[1]);
 
-	if (argc != 2)
+/**
+ * main - function to print out the change that needs to given
+ * @argc: number of arguments to be passed
+ * @argv: number of variables to calculate
+ * Return: always 0 for sucess
+ */
+
+int main(int argc, char **argv)
+{
+	int total, change;
+
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	while (rest != 0)
+
+	change = atoi(argv[1]);
+
+	for (total = 0; change > 0; total++)
 	{
-		if (rest < 0)
-			printf("0\n");
-		else if (rest >= 25)
-		{
-			rest -= 25;
-			result += 1;
-		}
-		else if (rest >= 10)
-		{
-			rest -= 10;
-			result += 1;
-		}
-		else if (rest >= 5)
-		{
-			rest -= 5;
-			result += 1;
-		}
-		else if (rest >= 2)
-		{
-			rest -= 2;
-			result += 1;
-		}
-		else if (rest >= 1)
-		{
-			rest -= 1;
-			result += 1;
-		}
+		if (change - 25 >= 0)
+			change = change - 25;
+		else if (change - 10 >= 0)
+			change = change - 10;
+		else if (change - 5 >= 0)
+			change = change - 5;
+		else if (change - 2 >= 0)
+			change = change - 2;
+		else if (change - 1 >= 0)
+			change = change - 1;
 	}
-	printf("%d\n", result);
+	printf("%d\n", total);
 	return (0);
 }

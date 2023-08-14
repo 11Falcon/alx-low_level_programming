3b07e92e1ea5b207d1b4ed2ea46e8c64a9b1643a
soufiane <soufian9mkns@gmail.com>
2023-08-14 13:24:33 -0700

    s

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - structure
+ * @name: char
+ * @age: float
+ * @type: char
+ */
+struct dog{
+        char *name;
+        float age;
+        char *owner;
+}

cdc3b2b8f8b7e17e3f394941767ee21ce81abefd
soufiane <soufian9mkns@gmail.com>
2023-08-14 15:12:27 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,6 +15,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
+	d->name = malloc(sizeof(name) + 1);
+        if (d->name == NULL)
+        {
+                free(d);
+                return (NULL);
+        }
+	d->owner = malloc(sizeof(owner) + 1);
+        if (d->owner == NULL)
+        {
+                free(d);
+                return (NULL);
+        }
 	d->name = name;
 	d->age = age;
 	d->owner = owner;

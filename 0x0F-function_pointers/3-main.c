e2e139fdcb1fb43685cbf9382b4a809b86876382
soufiane <soufian9mkns@gmail.com>
2023-08-16 16:03:23 -0700

    Falcon

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,41 @@
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "3-calc.h"
 /**
- * main - ...
- * @argc: ...
- * @argv: ...
+ * main - entry point for function
+ * @argc: arg counter, number of args
+ * @argv: array of pointers to args
  *
- * Return: ...
+ * Description: calculates basic arithmetic
+ *
+ * Return: 0 on success, 98 on failure
  */
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	char op;
+	int (*op_func)(int, int);
+	int result;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
-
-	oprt = get_op_func(argv[2]);
-
-	if (!oprt)
-	{
+	op = *(argv[2]);
+	if ((op == '/' || op == '%') && !atoi(argv[3]))
+	{ /* divide by zero exception */
 		printf("Error\n");
-		exit(99);
+		return (100);
 	}
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	/* actually do the operation now that we've error chcked */
+	op_func = get_op_func(&op);
+	if (op_func == NULL || argv[2][1] != '\0')
+	{ /* didn't find operator, or operator longer than 1 byte */
+		printf("Error\n");
+		return (99);
+	}
+	result = op_func(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", result);
 	return (0);
 }

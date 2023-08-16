c435f7e738582d2c3f843297e3a3d45884b6b62b
soufiane <soufian9mkns@gmail.com>
2023-08-16 08:34:24 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - Entry code
+ * @array: input
+ * @size: input
+ * @action: input
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}

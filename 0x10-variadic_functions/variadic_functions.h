4e634a6f414b1e2af57775ee262580ccba240cf0
soufiane <soufian9mkns@gmail.com>
2023-08-17 11:22:31 -0700

    3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,13 @@
 #ifndef F
 #define F
+
+struct type
+{
+char *character;
+void (*printf)(va_list);
+};
+typedef struct type variable_type;
+
 int _putchar(char c);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);

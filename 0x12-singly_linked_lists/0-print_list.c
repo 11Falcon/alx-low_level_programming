894b6c9a9b1eeb034155aaf3a35c048ac9cea97a
soufiane <soufian9mkns@gmail.com>
2023-08-24 14:53:31 -0700

    Printing list Structure

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_list - Entry code
+ * @list_t: input
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			i++;
+		}
+		else
+		{	
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+			i++;
+		}
+	}
+	return (i);
+}
+
+

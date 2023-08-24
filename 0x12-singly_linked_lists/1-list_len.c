5d0fd98233177fc93782c29ad9bf2e457a04c933
soufiane <soufian9mkns@gmail.com>
2023-08-24 15:52:15 -0700

    printing the number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * size_t - Entry code
+ * @h: input
+ * Return: size_t
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	printf("-> %lu elements\n", i);
+	return (i);
+}

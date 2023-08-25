edfc6a169a3ed9075ef1b059ac22ca526ee9c050
soufiane <soufian9mkns@gmail.com>
2023-08-25 11:45:14 -0700

    Printing a saying

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_it - Entry code
+ */
+void print_it(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

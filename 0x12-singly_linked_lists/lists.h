661151b9defd391bba61f56ad0112063e5e7297d
soufiane <soufian9mkns@gmail.com>
2023-08-25 12:47:08 -0700

    __attribute__((constructor));

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,5 +23,5 @@ list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *s);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void print_it(void);
+void print_it(void)__attribute__((constructor));
 #endif

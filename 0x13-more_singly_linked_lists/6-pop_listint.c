bd0704e58f42404a94c5d55a0a4bd2ebff0a0f75
soufiane <soufian9mkns@gmail.com>
2023-08-28 13:21:21 -0700

    remove the first node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,18 +2,20 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
- * pop_listint remove the first node
+ * pop_listint - remove the first node
  * @head: input
  * Return: int
  */
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int l;
 
 	if (!*head)
 		return (0);
 	temp = (*head)->next;
+	l = (*head)->n;
 	free(*head);
 	(*head) = temp;
-	return (temp->n);
+	return (l);
 }

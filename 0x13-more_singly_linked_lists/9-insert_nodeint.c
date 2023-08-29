2857da68be62764a754d8597a77d5d402d2ce594
soufiane <soufian9mkns@gmail.com>
2023-08-29 02:17:45 -0700

    inserting a new node to a specific position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,7 +7,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node = malloc(sizeof(listint_t));
 	unsigned int i = 0;
 
-	if(!new_node)
+	if (!new_node)
 		return (NULL);
 	new_node->n = n;
 	while ((idx - 1) != i)

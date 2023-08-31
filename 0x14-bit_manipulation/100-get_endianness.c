1ad013e2aaed7398e9e4cdec70fb7d725c2578c6
soufiane <soufian9mkns@gmail.com>
2023-08-31 15:19:50 -0700

    endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,11 +2,12 @@
 
 /**
  * get_endianness - Checks the endianness of the system.
+ * Return: int 
  */
 int get_endianness(void)
 {
 	unsigned int n = 1;
 	char *endian = (char *)&n;
-	
+
 	return (*endian == 1);
 }

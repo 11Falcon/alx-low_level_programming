569d35b71bfb9ed2e29f1fc9e95998e329af5324
soufiane <soufian9mkns@gmail.com>
2023-08-31 14:53:57 -0700

    setting a bit to 1 at index position

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * set_bit - setting a bit to 1
+ * @n: input
+ * @index: input
+ * Return: int
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	*n |= (1UL << index);
+	return (1);
+}

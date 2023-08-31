bb0c3d81d7d5051c6a7c00bbefc0a05e0baae1ea
soufiane <soufian9mkns@gmail.com>
2023-08-31 13:00:38 -0700

    convert a bit to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	len = 0;
 	while (b[len])
 	{
-		if(b[len] != '0' && b[len] != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
 		len++;
 	}

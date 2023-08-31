0522c04124877fee727ee0579f0ba2b21e9941ac
soufiane <soufian9mkns@gmail.com>
2023-08-31 14:34:28 -0700

    get bit on index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,14 @@
+#include "main.h"
+/**
+ * get_bit - get bit in index
+ * @n: input
+ * @index: input
+ * Return: int
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index >= sizeof(unsigned long int) * 8)
+		retutn (-1);
 	if (n & (1 << index))
 		return (1);
 	else

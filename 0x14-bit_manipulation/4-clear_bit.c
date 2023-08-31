4892e4b2c2eeed089cdfeceea3c4accc4f971d5a
soufiane <soufian9mkns@gmail.com>
2023-08-31 15:01:47 -0700

    setting a bit to 0 at index position

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * clear_bit - set it to 0
+ * @n: input
+ * @index: input
+ * Return: int
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	*n &= (0UL << index);
+	return (1);
+}

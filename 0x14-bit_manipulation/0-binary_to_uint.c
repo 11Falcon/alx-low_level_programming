d2e4f51e96cac2f9b4a4cd86f94582a6724468a5
soufiane <soufian9mkns@gmail.com>
2023-08-31 12:59:54 -0700

    convert a bit to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * binary_to_uint - Clear
+ * @b: pointer of char
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int number = 0;
+	unsigned int c = 1;
+	int cov, i, len;
+
+	if (!b)
+		return (0);
+	len = 0;
+	while (b[len])
+	{
+		if(b[len] != '0' && b[len] != '1')
+			return (0);
+		len++;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		cov = b[i] - '0';
+		number += cov * c;
+		c = c * 2;
+	}
+	return (number);
+}

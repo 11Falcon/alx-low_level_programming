06bfb33ff936e0075d0abf252c547d2ea0279da4
soufiane <soufian9mkns@gmail.com>
2023-10-14 10:45:15 -0700

    adding a node to the beggining

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (!head)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new = NULL)
+		return (NULL);
+	new->prev = NULL;
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	if (new->next != NULL)
+		(new->next)->prev =new;
+	return (new);
+}
+

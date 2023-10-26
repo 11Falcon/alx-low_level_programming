9757cc49aeb42dbfb8e2d6940112e931c443183d
soufiane <soufian9mkns@gmail.com>
2023-10-26 01:18:37 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,51 @@
+#include "main.c"
+/**
+ * add - adding two numbers
+ * @a: first numb
+ * @b: second numb
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * mul - multiply two numbers
+ * @a: firt num
+ * @b: second num
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * sub - subscri b from a
+ * @a: first
+ * @b: second
+ * Return: sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * div - div
+ * @a: first input
+ * @b: second input
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - mod
+ * @a: a
+ * @b: b
+ * Return: a%b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}

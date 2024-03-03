ef2497be57237043db7f974f04a5da334809223e
Falcon <soufian9mkns@gmail.com>
2024-03-03 02:58:37 -0800

    add a node to the hash table 2

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,6 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 		node->key = (char *)key;
 		node->value = (char *)value;
+		ht->array[index] = node;
 		return (1);
 	}
 	else

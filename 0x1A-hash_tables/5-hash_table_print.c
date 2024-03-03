63597400ea8398c5a589e51209a8ad6567003a00
Falcon <soufian9mkns@gmail.com>
2024-03-03 05:36:42 -0800

    retreiving the table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print the hash table
+ * @ht: the printed hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned int i = 0;
+	int first = 1;
+
+	printf("{");
+	if (!ht || ht->size == 0)
+	{
+		printf("1");
+		printf("}\n");
+		return;
+	}
+	node = ht->array[0];
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s' : '%s'", node->key, node->value);
+			first = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}

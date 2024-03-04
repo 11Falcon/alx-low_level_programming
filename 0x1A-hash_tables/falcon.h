3a0028e09eace9d7b0044fe7b1517d741f2a9479
Falcon <soufian9mkns@gmail.com>
2024-03-04 03:34:54 -0800

    delete a hash_table

--- 0x1A-hash_tables/falcon.h
+++ 0x1A-hash_tables/falcon.h
@@ -1,12 +1,12 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
+#ifndef _HashTables_
+#define _HashTables_
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdio.h>
 /**
  * struct hash_node_s - Node of a hash table
+ *
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
@@ -21,6 +21,7 @@ typedef struct hash_node_s
 
 /**
  * struct hash_table_s - Hash table data structure
+ *
  * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
@@ -31,7 +32,6 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
-
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -82,4 +82,4 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
-#endif /* HASH_TABLES_H */
+#endif/*Hash_Table*/
